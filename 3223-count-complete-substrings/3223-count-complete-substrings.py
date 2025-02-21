1c25b5a88090c921bb0272ab207f3488ab701b26
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-02-21 09:08:51 +0700

    Time: 6662 ms (18%), Space: 23.4 MB (16%) - LeetHub

---
+++ 3223-count-complete-substrings/3223-count-complete-substrings.py
@@ -0,0 +1,34 @@
+class Solution:
+    def countCompleteSubstrings(self, w: str, k: int) -> int:
+        def calc(s):
+            res = 0
+            v = len(s)
+            for i in range(1, 27):
+                if i * k > v: break
+                l = i * k
+                cnt = Counter(s[:l])
+                freq = Counter(cnt.values())
+                
+                if freq[k] == i: res += 1
+                
+                for idx in range(v - l):
+                    freq[cnt[s[idx]]] -= 1
+                    cnt[s[idx]] -= 1
+                    freq[cnt[s[idx]]] += 1
+
+                    freq[cnt[s[idx+l]]] -= 1
+                    cnt[s[idx+l]] += 1
+                    freq[cnt[s[idx+l]]] += 1
+
+                    if freq[k] == i: res += 1
+            return res
+        
+        idx = 0
+        ans = 0
+        n = len(w)
+        for i in range(1, n):
+            if abs(ord(w[i]) - ord(w[i-1])) > 2:
+                ans += calc(w[idx:i])
+                idx = i
+        ans += calc(w[idx:])
+        return ans
