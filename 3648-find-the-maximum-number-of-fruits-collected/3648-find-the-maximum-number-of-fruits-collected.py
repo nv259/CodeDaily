57219bed6ded921c19ba11f412e5a8895dceded1
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-08-08 13:21:22 +0700

    Time: 500 ms (85.48%), Space: 91.3 MB (93.55%) - LeetHub

---
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxCollectedFruits(self, fruits: List[List[int]]) -> int:
+        n = len(fruits)
+        res = sum(fruits[i][i] for i in range(n))
+
+        def dp():
+            prev = [float("-inf")] * n
+            curr = [float("-inf")] * n
+            prev[n - 1] = fruits[0][n - 1]
+
+            for i in range(1, n - 1):
+                for j in range(max(n - 1 - i, i + 1), n):
+                    best = prev[j]
+                    if j - 1 >= 0:
+                        best = max(best, prev[j - 1])
+                    if j + 1 < n:
+                        best = max(best, prev[j + 1])
+                    curr[j] = best + fruits[i][j]
+                prev, curr = curr, prev
+
+            return prev[n - 1]
+        
+        res += dp()
+
+        # Flip the matrix
+        for i in range(n):
+            for j in range(i):
+                fruits[i][j], fruits[j][i] = fruits[j][i], fruits[i][j]
+        
+        res += dp()
+        
+        return res
