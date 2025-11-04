f3d4547009ff81d807c5ae3ba3e879cce5a3adb4
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-11-04 16:59:09 +0700

    Time: 19 ms (14.84%), Space: 18.5 MB (19.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -0,0 +1,9 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s = [c for c in s]
+        t = [c for c in t]
+        s.sort()
+        t.sort()
+        
+        return s == t
+        
