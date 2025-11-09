edef31d6ec8010dff0d5b101b9008808657533b6
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-11-09 14:22:06 +0700

    Time: 249 ms (52.96%), Space: 19.9 MB (95.36%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,21 +1,30 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
-        
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        num_islands = 0
-        
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+        if not grid: return 0
+        m = len(grid)
+        n = len(grid[0])
+
+        def in_grid(i, j):
+            return 0 <= i < m and 0 <= j < n
+
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        def dfs(i, j):
+            grid[i][j] = '0'
+
+            for di, dj in directions:
+                mi = i + di
+                mj = j + dj
+
+                if in_grid(mi, mj) and grid[mi][mj] == '1':
+                    dfs(mi, mj) 
+            
+
+        ans = 0 
+        for i in range(m):
+            for j in range(n):
                 if grid[i][j] == '1':
-                    num_islands += 1
-                    queue = deque([(i, j)])
-                    while queue:
-                        x, y = queue.popleft()
-                        if 0 <= x < len(grid) and 0 <= y < len(grid[0]) and grid[x][y] == '1':
-                            grid[x][y] = '0'  # mark as visited
-                            for dx, dy in directions:
-                                queue.append((x + dx, y + dy))
-        
-        return num_islands 
+                    ans += 1
+                    dfs(i, j)
+
+        return ans
+
