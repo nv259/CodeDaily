8383062d34903ced348e1e8de1c1388c21e27b8a
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-11-13 16:01:12 +0700

    Time: 0 ms (100%), Space: 17.8 MB (76.84%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.py
@@ -1,13 +1,14 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        n = len(prices)
-        res = []
+        # monotonic decreasing stack
+        stack = []
 
         for i, price in enumerate(prices):
-            for j in range(i + 1, n):
-                if price >= prices[j]:
-                    price -= prices[j]
-                    break
-            res.append(price)         
-            
-        return res
+            while len(stack) and price <= prices[stack[-1]]:
+                j = stack.pop()
+                prices[j] -= price
+
+            stack.append(i)
+             
+        return prices 
+        
