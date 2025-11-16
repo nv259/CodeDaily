c61ea831ca87c2abfe5677a9c78474d8e19231ff
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2025-11-16 21:24:05 +0700

    Time: 0 ms (100%), Space: 21.3 MB (27.11%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.py
@@ -4,19 +4,39 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+def assign_no_lr_children(node):
+    if node is None: return 0
+
+    node.left_cnt = 0
+    node.right_cnt = 0
+
+    if node.left:
+        node.left_cnt = assign_no_lr_children(node.left)
+    if node.right:
+        node.right_cnt = assign_no_lr_children(node.right)
+    
+    return node.left_cnt + node.right_cnt + 1
+
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
-        arr = []
-        
-        def traverse_depth_first(node):
-            if node.left:
-                traverse_depth_first(node.left)
-            arr.append(node.val)
-            if node.right:
-                traverse_depth_first(node.right)
 
-        traverse_depth_first(root)
+        assign_no_lr_children(root)
 
-        return arr[k - 1]
+        def find_kth_smallest(node, k):
+            # Miss
+            if node is None: return None
+
+            # Hit
+            if k == node.left_cnt + 1:
+                return node.val 
+            
+            # Branching
+            if k <= node.left_cnt:
+                return find_kth_smallest(node.left, k)
+            else:
+                return find_kth_smallest(node.right, k - node.left_cnt - 1)
+        
+        return find_kth_smallest(root, k)
+
         
