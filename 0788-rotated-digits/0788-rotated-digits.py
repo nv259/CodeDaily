80c11082d417ad09da6cd087fa522dd9ac508406
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2026-05-02 08:26:27 +0700

    Time: 0 ms (100%), Space: 19.5 MB (14.49%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,19 +1,20 @@
+rotated_digits = {
+    '0': '0', '1': '1', '2': '5', 
+    '5': '2', '6': '9', '8': '8', '9': '6'
+}
+good_cnt = [0] * (10**4 + 1)
+
+def is_good_number(num):
+    if not all(c in rotated_digits for c in str(num)):
+        return False
+
+    rotated_num = int(''.join([rotated_digits[c] for c in str(num)]))
+    return rotated_num != num
+
+for num in range(1, 10**4 + 1):
+    good_cnt[num] = good_cnt[num - 1] + is_good_number(num)
+
+
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        rotated_digits = {
-            '0': '0', '1': '1', '2': '5', 
-            '5': '2', '6': '9', '8': '8', '9': '6'
-        }
-
-        def is_good_number(num):
-            if not all(c in rotated_digits for c in str(num)):
-                return False
-
-            rotated_num = int(''.join([rotated_digits[c] for c in str(num)]))
-            return rotated_num != num
-
-        good_cnt = 0
-        for num in range(1, n + 1):
-            good_cnt += is_good_number(num)
-        
-        return good_cnt
+        return good_cnt[n]
